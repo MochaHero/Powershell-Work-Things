cb5bff6a5b4f703624688d912705d636cf48a467
Mike <44067949+MochaHero@users.noreply.github.com>
2018-12-11 16:03:41 -0600

    Add files via upload

---
+++ Deep Freeze/Query-DeepFreezeState.ps1
@@ -0,0 +1,22 @@
+﻿#### Used to check if Deep Freeze is Frozen or Thawed on Public computers ####
+##Computers must have PSRemoting Enabled
+
+$computers = "SM-PUB01"
+
+$command = "& C:\windows\syswow64\DFC.exe get /ISFROZEN"
+
+
+foreach ($computer in $computers){
+
+    Invoke-Command -ComputerName $computer -scriptblock {
+        $command
+            If ($LASTEXITCODE -eq 0){
+            write-host "$env:COMPUTERNAME is Thawed"
+        }
+            else{
+            write-host "$env:COMPUTERNAME is Frozen"
+            }
+
+    }
+
+}
