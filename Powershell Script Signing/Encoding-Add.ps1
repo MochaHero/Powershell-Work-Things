90f26b1b788a59bac8a5e0f21218f6cf408fdbc7
Mike <44067949+MochaHero@users.noreply.github.com>
2018-12-12 09:33:27 -0600

    Add files via upload

---
+++ Powershell Script Signing/Encoding-Add.ps1
@@ -0,0 +1,15 @@
+﻿# Define your script as a script-block, with full functional syntax highlighting and line feeds
+$script = {
+
+Write-host "Hello!"
+
+}
+
+# Convert your script to a string
+$command = $script.ToString()
+
+# Use the conversion from the bottom of "Get-Help about_powershell.exe"
+$bytes = [System.Text.Encoding]::Unicode.GetBytes( $command )
+$encodedCommand = [Convert]::ToBase64String( $bytes )
+
+$encodedCommand
---
+++ Powershell Script Signing/Encoding-Remove.ps1
@@ -0,0 +1,4 @@
+﻿$EncodedText = “DQAKAA0ACgBXAHIAaQB0AGUALQBoAG8AcwB0ACAAIgBIAGUAbABsAG8AIQAiAA0ACgANAAoA"
+$DecodedText = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($EncodedText))
+$DecodedText
+
