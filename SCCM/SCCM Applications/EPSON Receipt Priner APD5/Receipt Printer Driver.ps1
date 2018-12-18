4437a44fd0d7b377145a8605821581dfe9c3e2ff
MochaHero <44067949+MochaHero@users.noreply.github.com>
2018-12-18 12:23:50 -0600

    Added SCCM Applications

---
+++ SCCM/SCCM Applications/EPSON Receipt Priner APD5/Receipt Printer Driver.ps1
@@ -0,0 +1,87 @@
+﻿<# Installation of APD 5 Receipt Printer
+This script will uninstal any existing APD 4 or 5 Receipt printer installed on the computer and instal the most up to date drivers with the custom settings
+
+Location for APD 5 install logs: C:\ProgramData\EPSON\EPSON Advanced Printer Driver 5\CopyInstallLog\CopyInstallLog.txt
+Location for APD 4 install logs: "C:\Windows\Apd4Setup.log"
+
+#>
+#Set Variables for Get commands
+$getprinter = Get-printer
+$getprinterdriver = Get-PrinterDriver
+
+#Script Locations
+$ScriptPath = $MyInvocation.MyCommand.Path
+$ScriptDir = Split-Path $ScriptPath
+$log      = "$ScriptDir\ADP5Receipt-Install.log"
+$date     = Get-Date -Format "MM-dd-yyyy hh:mm:sstt"
+
+#### Find Epson Program GUIDs ####
+$installedprograms = get-wmiobject Win32_Product
+$Epson = $installedprograms | Where-Object {$_.name -match "EPSON*"}
+$EpsonGUIDs = $Epson | Select-Object IdentifyingNumber
+
+"---------------------  Script started at $date (MM-dd-yyyy hh:mm:sstt) ---------------------" + "`r`n" | Out-File $log -append
+
+### Begin Uninstall Receipt printer with  (APD 5 Receipt printer install) ###
+Write-Host "[INFO]`t Uninstalling Epson Advanced Printer Driver 5 if installed"
+    "$date [INFO]`t Uninstalling Epson Advanced Printer Driver" | Out-File $log -Append
+Start-Process "$ScriptDir\APD_510_T88V.exe" -ArgumentList "/s /uninstall /rN" -Verbose -wait | out-file $log -Append
+
+### Begin Uninstall Receipt printer with  (APD 4 Receipt printer install) ###
+Write-Host "[INFO]`t Uninstalling Epson Advanced Printer Driver 4 if installed"
+    "$date [INFO]`t Uninstalling Epson Advanced Printer Driver" | Out-File $log -Append
+Start-Process "$ScriptDir\APD_456dE.exe" -ArgumentList "/s /uninstall /rN" -Verbose -wait | out-file $log -Append
+
+
+
+### Removing Print queue for Receipt Printer ### 
+Write-Host "[INFO]`t Checking for Existing Receipt Printer queue"
+    "$date [INFO]`t [INFO]`t Checking for Existing Receipt Printer queue" | Out-File $log -Append
+if ($getprinter.name -match 'Receipt*'){
+    Write-Host "[INFO]`t Found Receipt Printer queue. Removing..."
+        "$date [INFO]`t Found Receipt Printer queue. Removing..." | Out-File $log -Append
+    Remove-Printer 'Receipt*';
+    Write-Host "[INFO]`t Successfully deleted Receipt printer Print queue"
+        "$date [INFO]`t Successfully deleted Receipt printer Print queue" | Out-File $log -append
+    }
+    
+    else{
+        Write-Host "[INFO]`t Print queue does not exist $error"
+            "$date [INFO]`t Print queue does not exist $error" | Out-File $log -append
+        $error.Clear()
+        }
+
+### Removing Receipt Printer Driver ###    
+Write-Host "[INFO]`t Checking for Existing EPSON Printer Driver"
+    "$date [INFO]`t Checking for Existing EPSON Printer Driver" | Out-File $log -Append
+if ($getprinterdriver.name -like '*EPSON TM*'){
+    Write-Host "[INFO]`t Found EPSON Printer Driver. Removing..."
+        "$date [INFO]`t Found Epson Printer Driver. Removing..." | Out-File $log -Append
+    Remove-PrinterDriver -name *EPSON TM* ;
+    Write-Host "[INFO]`t Successfully deleted EPSON Printer Driver"
+        "$date [INFO]`t Successfully deleted EPSON Printer Driver" | Out-File $log -append
+    }
+
+
+#### Begin MSI Uninstall for each Epson Program ####
+Write-Host "[INFO]`t Checking for existing EPSON Programs"
+    "$date [INFO]`t Checking for existing EPSON Programs" | Out-File $log -Append
+foreach ($EpsonProgram in $EpsonGUIDs.IdentifyingNumber){
+        Write-Host "[INFO]`t Found EPSON Program. Uninstalling $EpsonProgram"
+            "$date [INFO]`t Found EPSON Program. Uninstalling $EpsonProgram" | Out-File $log -Append
+	Start-Process -filepath "MSIExec.exe" -ArgumentList "/x $EpsonProgram /qn /l*v .\'$EpsonProgram'.log /norestart" -wait
+}
+
+
+#### Begin New Receipt printer installation (APD 5 Receipt printer install) ####
+Write-Host "[INFO]`t Begin Installation of new Receipt Printer"
+    "$date [INFO]`t Begin Installation of new Receipt Printer" | Out-File $log -Append
+Start-Process "$ScriptDir\Apd5 Receipt.exe" -Verbose -wait | out-file $log -Append
+Write-Host "[INFO]`t Installation of new Receipt Printer Driver Complete"
+    "$date [INFO]`t Installation of new Receipt Printer Driver Complete" | Out-File $log -Append
+
+    $LASTEXITCODE | Out-File $log -Append
+    Exit $LASTEXITCODE
+    
+     
+"---------------------  Script finished at $date (MM-dd-yyyy hh:mm:sstt) ---------------------" + "`r`n" | Out-File $log -append
---
+++ SCCM/SCCM Applications/Ultimaker Cura 3.6/CuraInstall.ps1
@@ -0,0 +1,150 @@
+﻿<###Installation for Ultimaker Cura 3.6.0###>
+<#ChangeLog
+12/4/18 - Created Script to install Ultimaker Cura 3.6.0
+            --Script will import Driver Certificates into the computer for Arduino devices.
+                Certs have to be under the "Arduino Certificates" folder on the root of the script location.
+            --Driver installation will begin after certificates are imported. Driver installation under "arduino" folder on the root of the script location.
+                Has to be done in order to prevent failure of installation. Driver Certificates have to be added to Trusted publishers or else drivers will not install.
+            --Script will check if existing firewal rules for Cura is already there before adding new ones to avoid duplication.
+            --Cura install will install silently. No messages will pop up.
+#>
+
+#Script Locations
+$ScriptPath = $MyInvocation.MyCommand.Path
+$ScriptDir = Split-Path $ScriptPath
+$log      = "$ScriptDir\UltiCura-3.6.0-install.log"
+$date     = Get-Date -Format "MM-dd-yyyy hh:mm:sstt"
+
+#Certificate Locations
+$certPutLocation = "Cert:\LocalMachine\TrustedPublisher"
+$certFile = (Get-ChildItem -Path "$ScriptDir\Arduino Certificates\*" -Filter *.cer)
+$certFilecheck = (Test-Path -Path "$ScriptDir\Arduino Certificates\*" -Include *.cer)
+
+#Finding Firewall Rules for Cura
+$getUDPFirewall = Get-NetFirewallPortFilter -Protocol UDP | Get-netfirewallrule
+$domainUDP = $getUDPFirewall.displayname -match 'cura' -and $getUDPFirewall.Profile -match 'Domain'
+$publicUDP = $getUDPFirewall.displayname -match 'cura' -and $getUDPFirewall.Profile -match 'Public'
+
+$getTCPFirewall = Get-NetFirewallPortFilter -Protocol TCP | Get-netfirewallrule
+$domainTCP = $getTCPFirewall.displayname -match 'cura' -and $getTCPFirewall.Profile -match 'Domain'
+$publicTCP = $getTCPFirewall.displayname -match 'cura' -and $getTCPFirewall.Profile -match 'Public'
+
+
+"---------------------  Script started at $date (MM-dd-yyyy hh:mm:sstt) ---------------------" + "`r`n" | Out-File $log -append
+
+#### Import Driver Certs for Cura Software ####
+Write-host "[INFO]`t Importing Driver certificates.."
+    "$date [INFO]`t Importing Driver certificates.." | Out-File $log -Append
+
+if ($certFilecheck -eq $true) 
+{
+     Foreach ($cert in $certFile){
+        try{
+            $cert | Import-Certificate -CertStoreLocation $certPutLocation | Out-File $log -Append
+            Write-host "[INFO]`t $cert successfully imported."
+                "$date [INFO]`t $cert successfully imported." | Out-File $log -Append
+        }
+        catch {
+            Write-Error ("[ERROR]`t Error adding '$certfile' to '$certPutLocation': $error") -ErrorAction:Continue
+                "$date [ERROR]`t Error adding '$certfile' to '$certPutLocation': $error" | Out-File $log -Append
+                 $error.Clear()
+        }
+    }
+    
+}
+else {
+    Write-Host "[INFO]`t No Certificates found in specified directory"
+        "$date [INFO]`t No Certificates found in specified directory" | Out-File $log -Append
+}
+
+
+#### Begin Arduino Driver Install ####
+Write-host "[INFO]`t Begin Arduino Driver Install..."
+    "$date [INFO]`t Begin Arduino Driver Install..." | Out-File $log -Append
+
+    try {
+        Start-Process "$ScriptDir\arduino\dpinst-amd64.exe" -ArgumentList "/S /F" -Verbose -Wait | Out-File $log -Append
+    }
+    catch {
+         Write-Error ("[ERROR]`t Error installing Arduino Drivers. $error") -ErrorAction:Continue
+            "$date [ERROR]`t Error installing Arduino Drivers. $error" | Out-File $log -Append
+            $error.Clear()
+    }
+
+Write-Host "[INFO]`t Arduino Drivers Installed"
+
+
+#### Installing Firewall Rules for Cura Software ####
+
+Write-host "[INFO]`t Adding Firewal rules as needed..."
+    "$date [INFO]`t Adding Firewal rules as needed..." | Out-File $log -Append
+
+    ## DomainUDP Rule
+        if ($domainUDP -eq $true){
+            Write-Host "[INFO]`t Firewall Rule Already Exists"
+                "$date [INFO]`t Firewall Rule Already Exists" | Out-File $log -Append
+        }
+            else{
+                Write-Host "[INFO]`t Firewall Rule does not exist. Adding New Rule"
+                    "$date [INFO]`t Firewall Rule does not exist. Adding New Rule" | Out-File $log -Append
+                New-NetFirewallRule -DisplayName cura -Profile Domain -Enabled True -Action Allow -Program 'C:\Program Files\Ultimaker Cura 3.6\Cura.exe'-Protocol UDP | Out-File $log -Append
+            }
+    ## PublicUDP Rule
+        if ($publicUDP -eq $true){
+            Write-Host "[INFO]`t Firewall Rule Already Exists"
+                "$date [INFO]`t Firewall Rule Already Exists" | Out-File $log -Append
+        }
+            else{
+                Write-Host "[INFO]`t Firewall Rule does not exist. Adding New Rule"
+                    "$date [INFO]`t Firewall Rule does not exist. Adding New Rule" | Out-File $log -Append
+                New-NetFirewallRule -DisplayName cura -Profile Public -Enabled True -Action Block -Program 'C:\Program Files\Ultimaker Cura 3.6\Cura.exe'-Protocol UDP | Out-File $log -Append
+            }
+    ## DomainTCP Rule
+        if ($domainTCP -eq $true){
+            Write-Host "[INFO]`t Firewall Rule Already Exists"
+                "$date [INFO]`t Firewall Rule Already Exists" | Out-File $log -Append
+        }
+            else{
+                Write-Host "[INFO]`t Firewall Rule does not exist. Adding New Rule"
+                    "$date [INFO]`t Firewall Rule does not exist. Adding New Rule" | Out-File $log -Append
+                New-NetFirewallRule -DisplayName cura -Profile Domain -Enabled True -Action Allow -Program 'C:\Program Files\Ultimaker Cura 3.6\Cura.exe'-Protocol TCP | Out-File $log -Append
+            }
+    ## PublicTCP Rule
+        if ($publicTCP -eq $true){
+            Write-Host "[INFO]`t Firewall Rule Already Exists"
+                "$date [INFO]`t Firewall Rule Already Exists" | Out-File $log -Append
+        }
+            else{
+                Write-Host "[INFO]`t Firewall Rule does not exist. Adding New Rule"
+                    "$date [INFO]`t Firewall Rule does not exist. Adding New Rule" | Out-File $log -Append
+                New-NetFirewallRule -DisplayName cura -Profile Public -Enabled True -Action Block -Program 'C:\Program Files\Ultimaker Cura 3.6\Cura.exe'-Protocol TCP | Out-File $log -Append
+            }
+
+Write-Host "[INFO]`t Firewall Rules added successfully"
+    "$date [INFO]`t Firewall Rules added successfully" | Out-File $log -Append
+
+
+#### Begin Install for Ultimaker Cura 3.6.0 ####
+Write-Host "[INFO]`t Begin Ultimaker Cura 3.6.0 installation"
+    "$date [INFO]`t Begin Ultimaker Cura 3.6.0 installation" | Out-File $log -Append
+
+    try {
+        Start-Process "$ScriptDir\Ultimaker_Cura-3.6.0-win64.exe" -ArgumentList "/S"  -Verbose -Wait | Out-File $log -Append
+    }
+        catch {
+            Write-Error ("[ERROR]`t Error installing Ultimaker Cura 3.6.0. $error") -ErrorAction:Continue
+                "$date [INFO]`t Error installing Arduino Drivers. $error" | Out-File $log -Append
+                $error.Clear()
+        }
+
+
+Write-Host "[INFO]`t Ultimaker Cura 3.6.0 installation completed"
+    "$date [INFO]`t Ultimaker Cura 3.6.0 installation completed" | Out-File $log -Append
+
+$LASTEXITCODE | Out-File $log -Append
+Exit $LASTEXITCODE
+    
+     
+"---------------------  Script finished at $date (MM-dd-yyyy hh:mm:sstt) ---------------------" + "`r`n" | Out-File $log -append
+
+
